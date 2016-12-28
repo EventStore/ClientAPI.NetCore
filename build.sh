4c75412cc3386ab407b2f8dbc79aba8d06f6607f
James Geall <jageall@gmail.com>
2016-12-28 15:37:43 +0000

    Start projections for tests, tail the test logs, update build tools

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 dotnet restore src/EventStore.ClientAPI.NetCore/project.json
+dotnet restore test/EventStore.ClientAPI.NetCore.Tests/project.json
 dotnet build src/EventStore.ClientAPI.NetCore/project.json
+dotnet build test/EventStore.ClientAPI.NetCore.Tests/project.json
 dotnet pack src/EventStore.ClientAPI.NetCore/project.json
