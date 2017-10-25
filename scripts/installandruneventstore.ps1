4bc50f8978e32a1f245d19ad44ff5fe7678915b1
James Geall <jageall@gmail.com>
2017-10-25 15:05:27 +0300

    Updated eventstore server versions

--- scripts/installandruneventstore.ps1
+++ scripts/installandruneventstore.ps1
@@ -1,3 +1,3 @@
-(New-Object Net.WebClient).DownloadFile('http://download.geteventstore.com/binaries/EventStore-OSS-Win-v4.0.2.zip','c:\tools\eventstore.zip')
+(New-Object Net.WebClient).DownloadFile('https://eventstore.org/downloads/EventStore-OSS-Win-v4.0.3.zip','c:\tools\eventstore.zip')
 7z e C:\tools\eventstore.zip -oC:\tools\EventStore
 start-process -NoNewWindow "C:\tools\EventStore\EventStore.ClusterNode.exe" "--memdb --run-projections ALL"
