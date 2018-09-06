684d4f3f121ee81b3f744a7524b40cd179da377a
James Geall <jageall@gmail.com>
2018-09-06 05:59:22 +0300

    Remove Thread.Sleep from ClientAPI

--- test/EventStore.ClientAPI.NetCore.Tests/read_event_stream_backward_should.cs
+++ test/EventStore.ClientAPI.NetCore.Tests/read_event_stream_backward_should.cs
@@ -23,7 +23,7 @@ namespace EventStore.Core.Tests.ClientAPI
             using (var store = BuildConnection())
             {
                 store.ConnectAsync().Wait();
-                Assert.Throws<ArgumentOutOfRangeException>(() => store.ReadStreamEventsBackwardAsync(stream, 0, 0, resolveLinkTos: false));
+                Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => store.ReadStreamEventsBackwardAsync(stream, 0, 0, resolveLinkTos: false));
             }
         }
 
@@ -206,7 +206,7 @@ namespace EventStore.Core.Tests.ClientAPI
             {
                 store.ConnectAsync().Wait();
 
-                Assert.Throws<ArgumentException>(() => store.ReadStreamEventsBackwardAsync("foo", StreamPosition.Start, int.MaxValue, resolveLinkTos: false));
+                Assert.ThrowsAsync<ArgumentException>(() => store.ReadStreamEventsBackwardAsync("foo", StreamPosition.Start, int.MaxValue, resolveLinkTos: false));
 
             }
         }
