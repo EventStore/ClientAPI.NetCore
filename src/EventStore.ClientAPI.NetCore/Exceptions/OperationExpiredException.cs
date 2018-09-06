515fc18ddd1f4a347d50df2227e91806444ffedb
James Geall <jageall@gmail.com>
2018-09-06 23:24:51 +0100

    Add operation timeouts facility

---
+++ src/EventStore.ClientAPI.NetCore/Exceptions/OperationExpiredException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventStore.ClientAPI.Exceptions
+{
+    /// <summary>
+    /// Exception thrown if an operation expires before it can be scheduled. 
+    /// </summary>
+    public class OperationExpiredException : EventStoreConnectionException
+    {
+        /// <summary>
+        /// Constructs a new <see cref="OperationExpiredException"/>. 
+        /// </summary>
+        public OperationExpiredException()
+        {   
+        }
+
+        /// <summary>
+        /// Constructs a new <see cref="OperationExpiredException"/>. 
+        /// </summary>
+        /// <param name="message"></param>
+        public OperationExpiredException(string message) : base(message)
+        {   
+        }
+
+        /// <summary>
+        /// Constructs a new <see cref="OperationExpiredException"/>. 
+        /// </summary>
+        public OperationExpiredException(string message, Exception innerException):base(message, innerException)
+        {
+            
+        }
+    }
+}
