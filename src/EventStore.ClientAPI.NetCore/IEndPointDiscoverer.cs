72839a8d791049b6818b580563adf9863b8ac1f0
James Geall <jageall@gmail.com>
2018-09-07 10:17:47 +0100

    Add a new Create method to EventStoreNodeConnection to support custom IEndPointDiscoverer
    Fix typos in definitions

--- src/EventStore.ClientAPI.NetCore/IEndPointDiscoverer.cs
+++ src/EventStore.ClientAPI.NetCore/IEndPointDiscoverer.cs
@@ -1,17 +1,17 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 
-namespace EventStore.ClientAPI
+namespace EventStore.ClientAPI  
 {
     /// <summary>
-    /// Represents a way of discovering cluster endpoints this could be through gossip, consul, text files, etc 
+    /// Represents a way of discovering cluster endpoints. This could be through gossip, consul, text files, etc 
     /// </summary>
     public interface IEndPointDiscoverer
     {
         /// <summary>
         /// Called to discover a new <see cref="IPEndPoint"/> 
         /// </summary>
-        /// <param name="failedTcpEndPoint">The <see cref="IPEndPoint"/> The recently failed endpoint</param>
+        /// <param name="failedTcpEndPoint">The <see cref="IPEndPoint"/>The recently failed endpoint</param>
         /// <returns></returns>
         Task<NodeEndPoints> DiscoverAsync(IPEndPoint failedTcpEndPoint);
     }
